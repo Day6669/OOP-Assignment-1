e42aeb2cb8ef823df31982610af9c91c1f4273c7
Day <lunalunx10@gmail.com>
2023-02-10 17:25:19 +0000

    Add Card Dealing Functions

--- CMP1903M A01 2223/Pack.cs
+++ CMP1903M A01 2223/Pack.cs
@@ -8,27 +8,119 @@ namespace CMP1903M_A01_2223
 {
     class Pack
     {
-        List<Card> pack;
+
+        static private List<Card> pack;
+
+        private static Random random = new Random(0);
+        private static int RandRange(int from, int to)
+        {
+            return random.Next(from, to);
+        }
+
+        public enum CardFaces
+        {
+            Ace = 1,
+            Two = 2,
+            Three = 3,
+            Four = 4,
+            Five = 5,
+            Six = 6,
+            Seven = 7,
+            Eight = 8,
+            Nine = 9,
+            Ten = 10,
+            Jack = 11,
+            Queen = 12,
+            King = 13,
+        }
+
+        public enum ShuffleType
+        {
+            NoShuffle = 1,
+            RiffleShuffle =2,
+            FisherYatesShuffle = 3,
+        }
 
         public Pack()
         {
-            //Initialise the card pack here
+            //Creates the card pack here.
+            pack = new List<Card>();
+
+            foreach (var suit in Enum.GetValues(typeof(suitType)))
+            {
+                foreach (var face in Enum.GetValues(typeof(CardFaces)))
+                    //Loop to create card from all 13 cards from each suit.
+                {
+                    Card card = new Card();
+                    card.Suit = (suitType)suit;
+                    card.Value = (int)face;
+                    pack.Add(card);
+
+                }
+            }
+        }
+        public static void RiffleShuffle()
+        {
+
+            decimal half = pack.Count / 2;
+            int parts = (int)Math.Floor(half);
+            List<Card> shuffled = new List<Card>();
+
+            for (int i = 0; i < parts; i++)
+            {
+                shuffled.Add(pack[i]);
+                shuffled.Add(pack[parts + i]);
+            }
+            pack = shuffled;
         }
 
-        public static bool shuffleCardPack(int typeOfShuffle)
+        public static void FisherYatesShuffle()
+        {
+            for (int i = 0; i < pack.Count - 1; i++)
+            {
+                int randomIndex = RandRange(i, pack.Count);
+                Card temp = pack[i];
+                pack[i] = pack[randomIndex];
+                pack[randomIndex] = temp;
+            }
+        }
+
+        public static bool shuffleCardPack(ShuffleType typeOfShuffle)
         {
             //Shuffles the pack based on the type of shuffle
+            switch (typeOfShuffle)
+            {
+                case ShuffleType.NoShuffle:
+                    break;
 
+                case ShuffleType.RiffleShuffle:
+                    RiffleShuffle();
+                    break;
+
+                case ShuffleType.FisherYatesShuffle:
+                    FisherYatesShuffle();
+                    break;
+
+            }
+            return true;
         }
+
         public static Card deal()
         {
             //Deals one card
-
+            return pack[0];
 
         }
         public static List<Card> dealCard(int amount)
         {
             //Deals the number of cards specified by 'amount'
+            List<Card> items = new List<Card>();
+            for (int i = 0; i < amount; i++)
+            {
+                items.Add(pack[i]);
+            }
+
+            return items;
         }
     }
 }
--- CMP1903M A01 2223/Program.cs
+++ CMP1903M A01 2223/Program.cs
@@ -8,12 +8,28 @@ namespace CMP1903M_A01_2223
 {
     class Program
     {
+        static void PrintCard(Card card)
+        {
+            Console.WriteLine($"{card.Suit}-{card.Value}");
+        }
+
         static void Main(string[] args)
         {
+
             //Card card = new Card();
             //card.Suit = suitType.Diamonds;
             //card.Value = 12;
             //Console.WriteLine(card);
+            Pack pack = new Pack();
+            Pack.shuffleCardPack(Pack.ShuffleType.FisherYatesShuffle);
+            List<Card> cards = Pack.dealCard(10);
+
+            foreach (Card c in cards)
+            {
+                PrintCard(c);
+            }
+
+            Console.ReadLine();
         }
     }
 }
